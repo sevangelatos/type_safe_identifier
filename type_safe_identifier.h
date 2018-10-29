c133078407aa2d37dba7976d0e47b1dee12ba2fe
sevangelatos <sevangelatos@gmail.com>
2018-10-29 11:58:55 +0200

    Update type_safe_identifier.h

--- type_safe_identifier.h
+++ type_safe_identifier.h
@@ -2,24 +2,37 @@
 #define TYPE_SAFE_IDENTIFIER_H
 #include <ostream>
 
-template <typename T, typename IntegralT = int>
+/**
+ * A type-safe identifier class that wraps
+ * a primitive id type to create a type safe one.
+ */
+template <typename T, typename RawIdT = int>
 class TypeSafeIdentifier {
  public:
-  explicit TypeSafeIdentifier(IntegralT id) : id_(id) {}
+  explicit TypeSafeIdentifier(RawIdT id) : id_(id) {}
 
   /// Get the identifier value as an int
-  int value() { return id_; }
+  RawIdT value() { return id_; }
 
-  bool operator<(TypeSafeIdentifier<T> rhs) const { return id_ < rhs.id_; }
-  bool operator==(TypeSafeIdentifier<T> rhs) const { return id_ == rhs.id_; }
-  bool operator!=(TypeSafeIdentifier<T> rhs) const { return id_ != rhs.id_; }
+  bool operator<(TypeSafeIdentifier<T, RawIdT> rhs) const {
+    return id_ < rhs.id_;
+  }
+
+  bool operator==(TypeSafeIdentifier<T, RawIdT> rhs) const {
+    return id_ == rhs.id_;
+  }
+
+  bool operator!=(TypeSafeIdentifier<T, RawIdT> rhs) const {
+    return id_ != rhs.id_;
+  }
 
  private:
-  IntegralT id_;
+  RawIdT id_;
 };
 
-template <typename T, typename IntegralT>
-std::ostream &operator<<(std::ostream &os, TypeSafeIdentifier<T, IntegralT> identifier) {
+template <typename T, typename RawIdT>
+std::ostream &operator<<(std::ostream &os,
+                         TypeSafeIdentifier<T, RawIdT> identifier) {
   return os << identifier.value();
 }
 
